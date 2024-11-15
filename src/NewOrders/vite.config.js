ce33fbd56e4f8e47fe89a288c5ce2d6c2756d7c2
keshavsoft <nknnkumar@yahoo.com>
2024-11-15 11:49:03 +0530

    folder changed

--- src/NewOrders/vite.config.js
+++ src/NewOrders/vite.config.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const SourceFolderName = "SourceCode";
-const SrcFolder = "src/Laundry/Branch/NewOrders";
+const SrcFolder = "src/NewOrders";
 
 const FrontEndDistFolder = "publicDir/Branch/NewOrders";
 
