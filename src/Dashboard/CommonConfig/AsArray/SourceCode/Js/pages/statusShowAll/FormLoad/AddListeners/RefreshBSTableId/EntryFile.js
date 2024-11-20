e63c094a91924bd4ca4725a560ad05325b585ec4
pushpadamalanka82 <y*********@gmail.com>
2024-11-20 12:43:05 +0500

    onclicks and column

--- src/Dashboard/CommonConfig/AsArray/SourceCode/Js/pages/statusShowAll/FormLoad/AddListeners/RefreshBSTableId/EntryFile.js
+++ src/Dashboard/CommonConfig/AsArray/SourceCode/Js/pages/statusShowAll/FormLoad/AddListeners/RefreshBSTableId/EntryFile.js
@@ -1,11 +1,17 @@
 import { StartFunc as StartFuncFuncToRun } from "./ButtonClickFunc.js";
 
 let StartFunc = () => {
-    let jVarLocalRefreshBSTableId = document.getElementById("RefreshBSTableId");
+    // let jVarLocalRefreshBSTableId = document.getElementById("RefreshBSTableId");
 
-    if (jVarLocalRefreshBSTableId === null === false) {
-        jVarLocalRefreshBSTableId.addEventListener("click", StartFuncFuncToRun);
+    const arrClass = document.querySelectorAll(".RefreshBSTableClass");
+
+    for (let i of arrClass) {
+        i.addEventListener("click", StartFuncFuncToRun);
     };
+
+    // if (jVarLocalRefreshBSTableId === null === false) {
+    //     jVarLocalRefreshBSTableId.addEventListener("click", StartFuncFuncToRun);
+    // };
 };
 
 export { StartFunc };
--- src/Today/QrCodes/SourceCode/Js/pages/QrAllStatus/FormLoad/BuildBsTable/EntryFile.js
+++ src/Today/QrCodes/SourceCode/Js/pages/QrAllStatus/FormLoad/BuildBsTable/EntryFile.js
@@ -1,8 +1,10 @@
 import optionsJson from './options.json' with {type: 'json'};
 import { StartFunc as StartFuncOnClickRowFunc } from "./onClickRow/EntryFile.js";
+import { StartFunc as ForColumns } from "./ForColumns/entryFile.js";
 
 const StartFunc = () => {
     var $table = $('#table');
+    ForColumns({ inColumns: optionsJson.columns });
     optionsJson.onClickRow = StartFuncOnClickRowFunc;
     $table.bootstrapTable(optionsJson);
 };
