b637fbdcafb289a00971a32b47d47b92130b981a
Achansandeep21 <93124032+Achansandeep21@users.noreply.github.com>
2024-11-22 17:46:38 +0530

    Date issue

--- src/NewOrders/SourceCode/Js/pages/AddItems/FormLoad/BuildBSTables/ItemsTable/EntryFile.js
+++ src/NewOrders/SourceCode/Js/pages/AddItems/FormLoad/BuildBSTables/ItemsTable/EntryFile.js
@@ -16,12 +16,19 @@ const StartFunc = () => {
 };
 
 const jFdate = () => {
-    var sevenDaysAgo = new Date();
-    sevenDaysAgo.setDate(sevenDaysAgo.getDate() + 7);
+    // var sevenDaysAgo = new Date();
+    // sevenDaysAgo.setDate(sevenDaysAgo.getDate() + 7);
 
-    var formattedSevenDaysAgo = sevenDaysAgo.getFullYear() + '-' + ((sevenDaysAgo.getMonth() + 1) < 10 ? '0' : '') + (sevenDaysAgo.getMonth() + 1) + '-' + (sevenDaysAgo.getDate() < 10 ? '0' : '') + sevenDaysAgo.getDate();
+    // var formattedSevenDaysAgo = sevenDaysAgo.getFullYear() + '-' + ((sevenDaysAgo.getMonth() + 1) < 10 ? '0' : '') + (sevenDaysAgo.getMonth() + 1) + '-' + (sevenDaysAgo.getDate() < 10 ? '0' : '') + sevenDaysAgo.getDate();
 
-    document.getElementById('TableFooterdateInputId').value = formattedSevenDaysAgo;
+    // document.getElementById('TableFooterdateInputId').value = formattedSevenDaysAgo;
+    var today = new Date(); 
+    var dd = String(today.getDate()).padStart(2, '0'); 
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+    var yyyy = today.getFullYear(); 
+    today = yyyy + '-' + mm + '-' + dd; 
+    document.getElementById("TableFooterdateInputId").setAttribute("min", today); 
+    document.getElementById("TableFooterdateInputId").value = today; 
 };
 
 export { StartFunc };
