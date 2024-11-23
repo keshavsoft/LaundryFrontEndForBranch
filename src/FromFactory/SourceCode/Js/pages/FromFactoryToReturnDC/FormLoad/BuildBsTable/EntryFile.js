9878836df979c104e3a13cbad839fb5801b5a4e8
sharath0036 <ksharathbabu2@gmail.com>
2024-11-23 17:48:34 +0530

    new Ui FromFactory

--- src/FromFactory/SourceCode/Js/pages/FromFactoryToReturnDC/FormLoad/BuildBsTable/EntryFile.js
+++ src/FromFactory/SourceCode/Js/pages/FromFactoryToReturnDC/FormLoad/BuildBsTable/EntryFile.js
@@ -3,10 +3,13 @@ import UrlJson from './Url.json' with {type: 'json'};
 import { StartFunc as StartFuncOnClickRowFunc } from "./onClickRow/EntryFile.js";
 import { StartFunc as queryParams } from "./queryParams/EntryFile.js";
 import { StartFunc as responseHandler } from "./responseHandler/EntryFile.js";
+import { StartFunc as ForColumns } from "./ForColumns/EntryFile.js";
+
 let jVarLocalFactoryName = localStorage.getItem("BranchName");
 
 const StartFunc = () => {
     var $table = $('#table');
+    ForColumns({ inColumns: optionsJson.columns });
     optionsJson.onClickRow = StartFuncOnClickRowFunc;
     optionsJson.responseHandler = responseHandler;
     optionsJson.queryParams = queryParams;
--- src/FromFactory/SourceCode/Js/pages/FromFactoryToReturnDC/FormLoad/BuildBsTable/ForColumns/EntryFile.js
+++ src/FromFactory/SourceCode/Js/pages/FromFactoryToReturnDC/FormLoad/BuildBsTable/ForColumns/EntryFile.js
@@ -1,7 +1,29 @@
-import ColumnsJson from './columns.json' with {type: 'json'};
+import { StartFunc as ColumnSerial } from "./ColumnSerial/entryFile.js";
+import { StartFunc as Duration } from "./Duration/entryFile.js";
+import { StartFunc as Items } from "./Items/entryFile.js";
+import { StartFunc as Qrgenerate } from "./Qrgenerate/entryFile.js";
 
-const StartFunc = () => {
-    return ColumnsJson;
+let StartFunc = ({ inColumns }) => {
+    let LocalColumns = inColumns;
+    let LocalColumnOperateFine = LocalColumns.find(element => element.field === "id")
+    let LocalColumnDuration = LocalColumns.find(element => element.field === "Duration");
+    let LocalColumnItems = LocalColumns.find(element => element.field === "ItemDetails");
+
+    if (LocalColumnOperateFine === undefined === false) {
+        ColumnSerial({ inFindColumn: LocalColumnOperateFine });
+    };
+    if (LocalColumnDuration === undefined === false) {
+        Duration({ inFindColumn: LocalColumnDuration });
+    };
+    if (LocalColumnItems === undefined === false) {
+        Items({ inFindColumn: LocalColumnItems });
+    };
+
+    let LocalColumnQrgenerate = LocalColumns.find(element => element.field === "Scan");
+   
+    if (LocalColumnQrgenerate === undefined === false) {
+        Qrgenerate({ inFindColumn: LocalColumnQrgenerate });
+    };
+ 
 };
-
 export { StartFunc };
