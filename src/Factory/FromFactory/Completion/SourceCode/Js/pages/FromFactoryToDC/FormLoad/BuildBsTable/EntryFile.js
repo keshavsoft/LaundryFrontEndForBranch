6857c6bea5e835213c390374337735ff188d6321
keshavsoft <nknnkumar@yahoo.com>
2024-12-07 14:40:44 +0530

    /ForColumns/entryFile.js case fixed

--- src/Factory/FromFactory/Completion/SourceCode/Js/pages/FromFactoryToDC/FormLoad/BuildBsTable/EntryFile.js
+++ src/Factory/FromFactory/Completion/SourceCode/Js/pages/FromFactoryToDC/FormLoad/BuildBsTable/EntryFile.js
@@ -3,7 +3,7 @@ import optionsJson from './options.json' with {type: 'json'};
 import { StartFunc as onClickRow } from "./onClickRow/EntryFile.js";
 import { StartFunc as queryParams } from "./queryParams/EntryFile.js";
 import { StartFunc as responseHandler } from "./responseHandler/EntryFile.js";
-import { StartFunc as ForColumns } from "./ForColumns/entryFile.js";
+import { StartFunc as ForColumns } from "./ForColumns/EntryFile.js";
 
 const StartFunc = () => {
     var $table = $('#table');
