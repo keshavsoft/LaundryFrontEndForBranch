9791aabd7172c086476707e68f3b3ff18f1f8e24
pushpadamalanka82 <y*********@gmail.com>
2024-12-11 11:47:31 +0530

    changes

--- src/Factory/ToFactory/SourceCode/Js/pages/ToFactoryVouchers/FormLoad/BuildBsTable/ForColumns/EntryFile.js
+++ src/Factory/ToFactory/SourceCode/Js/pages/ToFactoryVouchers/FormLoad/BuildBsTable/ForColumns/EntryFile.js
@@ -1,7 +1,32 @@
-import ColumnsJson from './columns.json' with {type: 'json'};
+import { StartFunc as ColumnOperate } from "./ColumnSerial/entryFile.js";
+import { StartFunc as ColumnScan } from "./ColumnScan/entryFile.js";
+import { StartFunc as ItemsDetails } from "./ItemsDetails/entryFile.js";
+import { StartFunc as EntryScanCount } from "./EntryScanCount/entryFile.js";
 
-const StartFunc = () => {
-    return ColumnsJson;
+
+let StartFunc = ({ inColumns }) => {
+    let LocalColumns = inColumns;
+    let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial");
+
+    if (LocalColumnOperateFine === undefined === false) {
+        ColumnOperate({ inFindColumn: LocalColumnOperateFine });
+    };
+
+    let LocalScanColumn = LocalColumns.find(element => element.field === "Scan");
+
+    if (LocalScanColumn === undefined === false) {
+        ColumnScan({ inFindColumn: LocalScanColumn });
+    };
+    let LocalItemDetails = LocalColumns.find(element => element.field === "ItemDetails");
+
+    if (LocalItemDetails === undefined === false) {
+        ItemsDetails({ inFindColumn: LocalItemDetails });
+    };
+    let LocalEntryScanCount = LocalColumns.find(element => element.field === "EntryScanCount");
+
+    if (LocalEntryScanCount === undefined === false) {
+        EntryScanCount({ inFindColumn: LocalEntryScanCount });
+    };
 };
 
 export { StartFunc };
