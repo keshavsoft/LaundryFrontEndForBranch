b71f60b6d510c0026c2c2b6143496f57c9ba3b9b
sharath0036 <ksharathbabu2@gmail.com>
2024-12-12 11:32:18 +0530

    code

--- src/Delivery/ToChecking/SourceCode/Js/pages/CheckOrder/FormLoad/BuildBsTable/ForColumns/ColumnScan/entryFile.js
+++ src/Delivery/ToChecking/SourceCode/Js/pages/CheckOrder/FormLoad/BuildBsTable/ForColumns/ColumnScan/entryFile.js
@@ -1,14 +1,15 @@
 let StartFunc = ({ inFindColumn }) => {
-    inFindColumn.footerFormatter = priceFormatter;
+    inFindColumn.formatter = jVarLocalFormatterFunc;
 };
 
-function priceFormatter(data) {
-    var field = this.field
-    return '' + data.map(function (row) {
-        return +row.Rate
-    }).reduce(function (sum, i) {
-        return sum + i
-    }, 0)
-}
+let jVarLocalFormatterFunc = (value, row, index) => {
+    console.log("row:", row, value);
+
+    if (row.ProcessReturn === true || row.Completion === true || row.EntryReturn === true) {
+        return '<span class="badge bg-success"> Returned</span>';
+    } else {
+        return '<span class="badge bg-danger">Pending</span>';
+    };
+};
 
 export { StartFunc };
