0e3f1f67e597b00b92da5d95741c3815ff1a74c9
Ruthuja-09 <ruthuja.5d0@gmail.com>
2024-12-23 18:44:00 +0530

    Added Opts columns

--- src/NewOrders/SourceCode/Js/pages/AddItemsV2/FormLoad/BuildBSTables/ItemsTable/ForColumns/EntryFile.js
+++ src/NewOrders/SourceCode/Js/pages/AddItemsV2/FormLoad/BuildBSTables/ItemsTable/ForColumns/EntryFile.js
@@ -1,12 +1,14 @@
 import { StartFunc as ColumnOperate } from "./ColumnSerial/entryFile.js";
 import { StartFunc as ColumnRate } from "./ColumnRate/entryFile.js";
 import { StartFunc as StartFuncColumnAddOns } from "./ColumnAddOns/entryFile.js";
+import { StartFunc as StartFuncColumnopts } from "./Columnopts/entryFile.js";
 
 let StartFunc = ({ inColumns }) => {
     let LocalColumns = inColumns;
     let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial");
     let LocalColumnRate = LocalColumns.find(element => element.field === "Rate");
     let LocalColumnAddOns = LocalColumns.find(element => element.field === "KS-AddOns");
+    let LocalColumnopts = LocalColumns.find(element => element.field === "KS-Opts");
 
     if (LocalColumnOperateFine === undefined === false) {
         ColumnOperate({ inFindColumn: LocalColumnOperateFine });
@@ -18,6 +20,9 @@ let StartFunc = ({ inColumns }) => {
     
     if (LocalColumnAddOns === undefined === false) {
         StartFuncColumnAddOns({ inFindColumn: LocalColumnAddOns });
+    };
+    if (LocalColumnopts === undefined === false) {
+        StartFuncColumnopts({ inFindColumn: LocalColumnopts });
     };
 };
 
