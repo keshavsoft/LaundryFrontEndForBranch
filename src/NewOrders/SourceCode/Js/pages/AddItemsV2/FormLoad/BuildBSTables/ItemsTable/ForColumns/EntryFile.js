92e5778db30e3f9b4349ba746bee5ff886fc7739
keshavsoft <nknnkumar@yahoo.com>
2024-12-23 18:07:46 +0530

    ColumnAddOns working

--- src/NewOrders/SourceCode/Js/pages/AddItemsV2/FormLoad/BuildBSTables/ItemsTable/ForColumns/EntryFile.js
+++ src/NewOrders/SourceCode/Js/pages/AddItemsV2/FormLoad/BuildBSTables/ItemsTable/ForColumns/EntryFile.js
@@ -1,16 +1,23 @@
 import { StartFunc as ColumnOperate } from "./ColumnSerial/entryFile.js";
 import { StartFunc as ColumnRate } from "./ColumnRate/entryFile.js";
+import { StartFunc as StartFuncColumnAddOns } from "./ColumnAddOns/entryFile.js";
 
 let StartFunc = ({ inColumns }) => {
     let LocalColumns = inColumns;
     let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial");
     let LocalColumnRate = LocalColumns.find(element => element.field === "Rate");
+    let LocalColumnAddOns = LocalColumns.find(element => element.field === "KS-AddOns");
 
     if (LocalColumnOperateFine === undefined === false) {
         ColumnOperate({ inFindColumn: LocalColumnOperateFine });
     };
+
     if (LocalColumnRate === undefined === false) {
         ColumnRate({ inFindColumn: LocalColumnRate });
+    };
+    
+    if (LocalColumnAddOns === undefined === false) {
+        StartFuncColumnAddOns({ inFindColumn: LocalColumnAddOns });
     };
 };
 
