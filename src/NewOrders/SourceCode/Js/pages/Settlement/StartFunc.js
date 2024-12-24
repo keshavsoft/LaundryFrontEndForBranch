f464d972f166be1077645253845dc57f6efd6ec9
sharath0036 <ksharathbabu2@gmail.com>
2024-12-24 17:48:14 +0530

    logins fixed

--- src/NewOrders/SourceCode/Js/pages/Settlement/StartFunc.js
+++ src/NewOrders/SourceCode/Js/pages/Settlement/StartFunc.js
@@ -1,13 +1,15 @@
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
 import { StartFunc as LoginModal } from "../../../assets/static/js/Common/NiceAdmin/Header/LoginModal/EntryFile.js";
+import { StartFunc as StartFuncShowToDom } from "./ShowToDom/EntryFile.js";
 
 const StartFunc = () => {
-    let jVarLocalFromAdmin = LoginModal({ inSuccessFunc: StartFuncFormLoad });
-    //let jVarLocalFromAdmin = true;
+    let jVarLocalFromAdmin = LoginModal({ inSuccessFunc: StartFuncShowToDom });
 
     if (jVarLocalFromAdmin) {
         StartFuncFormLoad();
     };
+
+    StartFuncShowToDom()
 };
 
 StartFunc();
