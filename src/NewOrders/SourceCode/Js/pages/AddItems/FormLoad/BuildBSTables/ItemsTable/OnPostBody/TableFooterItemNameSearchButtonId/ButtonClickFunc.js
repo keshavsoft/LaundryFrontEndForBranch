a3c5c89e0fb6415bd22b3efd2459e410379d6a27
keshavsoft <nknnkumar@yahoo.com>
2024-12-25 15:00:31 +0530

    Need to work more on AddItems Modal for items

--- src/NewOrders/SourceCode/Js/pages/AddItems/FormLoad/BuildBSTables/ItemsTable/OnPostBody/TableFooterItemNameSearchButtonId/ButtonClickFunc.js
+++ src/NewOrders/SourceCode/Js/pages/AddItems/FormLoad/BuildBSTables/ItemsTable/OnPostBody/TableFooterItemNameSearchButtonId/ButtonClickFunc.js
@@ -1,17 +1,9 @@
 import { StartFunc as StartFuncItemsOnModalTable } from "./ItemsOnModalTable/EntryFile.js";
 
 const StartFunc = () => {
-    // let jVarLocalModalId = "ItemsOnModal";
     let jFLocalFilter = document.getElementById("TableFooterItemNameId").value;
 
     StartFuncItemsOnModalTable(jFLocalFilter);
-    // if(jFLocalFilter) {
-    //     StartFuncItemsWithSearchOnModalTable(jFLocalFilter);
-    //     //console.log(jFLocalFilter);
-    // }
-    // else {
-    //     StartFuncItemsOnModalTable();
-    // }
 };
 
 export { StartFunc };
