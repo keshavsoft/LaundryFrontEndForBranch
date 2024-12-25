ed6db654fd3be559a3381b74e0ab295118235e0f
keshavsoft <nknnkumar@yahoo.com>
2024-12-25 22:18:36 +0530

    Customer.html redirect url changed to config

--- src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/AddListeners/NewOrderButtonId/FetchFunc/AfterFetch/EntryFile.js
+++ src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/AddListeners/NewOrderButtonId/FetchFunc/AfterFetch/EntryFile.js
@@ -1,5 +1,8 @@
+import ConfigJson from '../../../../../Config.json' with {type: 'json'};
+
 let StartFunc = () => {
-    window.location = "AddItemsV1.html";
+    // window.location = "AddItemsV1.html";
+    window.location = ConfigJson.Urls.NewOrderButtonId.RedirectUrl['200Status'];
 };
 
 export { StartFunc };
