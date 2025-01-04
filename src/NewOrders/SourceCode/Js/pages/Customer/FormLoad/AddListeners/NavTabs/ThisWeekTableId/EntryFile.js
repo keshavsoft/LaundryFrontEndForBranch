0629959475a7833a785eaabab5a21f3509a5dbd2
sharath0036 <ksharathbabu2@gmail.com>
2025-01-04 14:02:47 +0530

    yesterday and week orders fixed

--- src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/AddListeners/NavTabs/ThisWeekTableId/EntryFile.js
+++ src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/AddListeners/NavTabs/ThisWeekTableId/EntryFile.js
@@ -4,7 +4,7 @@ let StartFunc = () => {
     let jVarLocalMasterButtonId = "ThisWeekRefreshButtonId";
     let jVarLocalHtmlId = document.getElementById(jVarLocalMasterButtonId);
 
-    jVarLocalHtmlId.addEventListener("click",StartFuncClickFunc);
+    jVarLocalHtmlId.addEventListener("click", StartFuncClickFunc);
 };
 
 export { StartFunc }
