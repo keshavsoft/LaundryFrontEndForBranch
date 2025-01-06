e6be1c4a389a8b52207dd55d3417ce530825c7aa
sharath0036 <ksharathbabu2@gmail.com>
2025-01-06 19:57:05 +0530

    date filter

--- src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/AddListeners/CustomerNameSearchButtonId/FetchFromFuncs/AfterFetch/EntryFile.js
+++ src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/AddListeners/CustomerNameSearchButtonId/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -1,11 +1,22 @@
 import { StartFunc as ToLocalStorage } from "../../../../../ToLocalStorage/AllOrders.js";
+
 let StartFunc = (inData) => {
     let jVarLocalData = inData.JsonData;
-    let jVarLocalOrdered = jVarLocalData.sort((x, y) => ((x.pk === y.pk) ? 0 : ((x.pk < y.pk) ? 1 : -1)));
-    let jVarLocalWithAggValues = jFLocalInsertAggValues({ inData: jVarLocalOrdered });
-    ToLocalStorage({ inOrdersArray: jVarLocalData });
 
-    var $table = $('#table')
+    let today = new Date().toISOString().split("T")[0];
+
+    let jVarLocalFilteredData = jVarLocalData.filter(order => {
+        
+        let orderDate = new Date(order.DateTime).toISOString().split("T")[0]; 
+        
+        return orderDate === today;
+    });
+
+    let jVarLocalOrdered = jVarLocalFilteredData.sort((x, y) => ((x.pk === y.pk) ? 0 : ((x.pk < y.pk) ? 1 : -1)));
+    let jVarLocalWithAggValues = jFLocalInsertAggValues({ inData: jVarLocalOrdered });
+    ToLocalStorage({ inOrdersArray: jVarLocalFilteredData });
+
+    var $table = $('#table');
 
     // $table.bootstrapTable({
     //     data: jVarLocalWithAggValues,
@@ -24,22 +35,22 @@ let jFLocalInsertAggValues = ({ inData }) => {
         element.AggValues = {};
         element.AggValues.ItemDetails = `${Object.keys(element.ItemsInOrder).length} / ${Object.values(element.ItemsInOrder).map(p => p.Pcs).reduce((acc, val) => acc + parseInt(val), 0)}`;
 
-        element.AggValues.SettlementAmount = ""
+        element.AggValues.SettlementAmount = "";
 
         if (Object.values(element.CheckOutData)[0]) {
             element.AggValues.SettlementAmount = Object.values(element.CheckOutData)[0].CardAmount + Object.values(element.CheckOutData)[0].CashAmount + Object.values(element.CheckOutData)[0].UPIAmount;
             element.IsSettled = false;
-        };
+        }
 
         if (Object.keys(element.CheckOutData).length > 0) {
             element.IsSettled = true;
-        };
+        }
 
         let jVarLocalAddOnArray = Object.values(element.AddOnData);
 
-        element.AggValues.ItemsArray = Object.values(element.ItemsInOrder).map(element => {
-            element.AddOnDataAsArray = jVarLocalAddOnArray.filter(LoopInside => LoopInside.AddOnItemSerial === element.ItemSerial);
-            return element;
+        element.AggValues.ItemsArray = Object.values(element.ItemsInOrder).map(item => {
+            item.AddOnDataAsArray = jVarLocalAddOnArray.filter(addOn => addOn.AddOnItemSerial === item.ItemSerial);
+            return item;
         });
 
         return element;
