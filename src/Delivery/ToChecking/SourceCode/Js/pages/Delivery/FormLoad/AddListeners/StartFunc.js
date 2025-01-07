a3553271e2548c60cc0ce025d84e3edbbe72af16
sharath0036 <ksharathbabu2@gmail.com>
2025-01-07 13:33:50 +0530

    delivery button

--- src/Delivery/ToChecking/SourceCode/Js/pages/Delivery/FormLoad/AddListeners/StartFunc.js
+++ src/Delivery/ToChecking/SourceCode/Js/pages/Delivery/FormLoad/AddListeners/StartFunc.js
@@ -1,7 +1,9 @@
 import { StartFunc as OrderInputId } from "./OrderInputId/keypress/EntryFile.js";
+import { StartFunc as SubmitId } from "./SubmitId/EntryFile.js";
 
 const StartFunc = () => {
     OrderInputId();
+    SubmitId();
 };
 
 export { StartFunc };
---
+++ src/Delivery/ToChecking/SourceCode/Js/pages/Delivery/FormLoad/AddListeners/SubmitId/AfterFetch/EntryFile.js
@@ -0,0 +1,10 @@
+let StartFunc = async () => {
+ Swal.fire({
+    title: 'Success',
+    text: 'Settelement Was Successed',
+    icon: 'Success',
+
+ })
+};
+
+export { StartFunc }
