8051c1723bd720aff70af6c58b43b20c9f0c8be2
pushpadamalanka82 <y*********@gmail.com>
2025-01-20 20:15:52 +0530

    changes

--- src/Factory/FromFactory/EntryReturn/SourceCode/Js/pages/ShowDCBranchItems/FormLoad/BuildBsTable/ForColumns/EntryFile.js
+++ src/Factory/FromFactory/EntryReturn/SourceCode/Js/pages/ShowDCBranchItems/FormLoad/BuildBsTable/ForColumns/EntryFile.js
@@ -1,10 +1,10 @@
 import { StartFunc as ColumnRate } from "./ColumnRate/entryFile.js";
 import { StartFunc as ColumnSerial } from "./ColumnSerial/entryFile.js";
+import { StartFunc as ColumnStatus } from "./ColumnStatus/entryFile.js";
 
 let StartFunc = ({ inColumns }) => {
     let LocalColumns = inColumns;
     let LocalColumnRate = LocalColumns.find(element => element.field === "Rate");
-
     if (LocalColumnRate === undefined === false) {
         ColumnRate({ inFindColumn: LocalColumnRate });
     };
@@ -12,6 +12,11 @@ let StartFunc = ({ inColumns }) => {
     let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial")
     if (LocalColumnOperateFine === undefined === false) {
         ColumnSerial({ inFindColumn: LocalColumnOperateFine });
+    };
+
+    let LocalColumnStatus = LocalColumns.find(element => element.field === "FFEntryStatus");
+    if (LocalColumnStatus === undefined === false) {
+        ColumnStatus({ inFindColumn: LocalColumnStatus });
     };
 };
 export { StartFunc };
