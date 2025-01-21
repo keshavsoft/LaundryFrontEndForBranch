c055723dbe014fd26c5a037fc3847582b0388220
sharath0036 <ksharathbabu2@gmail.com>
2025-01-21 16:43:23 +0530

    delete order

--- src/BranchToday/Branch/SourceCode/Js/pages/DeleteOrder/FormLoad/BuildBsTable/EntryFile.js
+++ src/BranchToday/Branch/SourceCode/Js/pages/DeleteOrder/FormLoad/BuildBsTable/EntryFile.js
@@ -1,10 +1,15 @@
 import optionsJson from './options.json' with {type: 'json'};
 import { StartFunc as StartFuncOnClickRowFunc } from "./onClickRow/EntryFile.js";
+import { StartFunc as ForColumns } from "./ForColumns/EntryFile.js";
 
 const StartFunc = () => {
     var $table = $('#table');
     optionsJson.onClickRow = StartFuncOnClickRowFunc;
+    
+    ForColumns({ inColumns: optionsJson.columns });
+
     $table.bootstrapTable(optionsJson);
+
 };
 
 export { StartFunc };
