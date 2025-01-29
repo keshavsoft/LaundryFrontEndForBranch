e62aae6a8c56af6e7a84e2bafac1e6cc3f432512
Srivasu215 <sri.vasu215@gmail.com>
2025-01-29 13:11:33 +0530

    CustomerV3

--- src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/BuildBSTables/ForColumns/Columnopts/entryFile.js
+++ src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/BuildBSTables/ForColumns/Columnopts/entryFile.js
@@ -1,21 +1,26 @@
 let StartFunc = ({ inFindColumn }) => {
     inFindColumn.formatter = jFLocalOptsFormater;
-    // inFindColumn.footerFormatter = jFLocalFooterFormatterFunc;
 };
 
 function jFLocalOptsFormater(value, row, index) {
-    console.log("row");
-    
-    return [
-        `<a href="./ExitOrderItems.html?OrderNumber=${row.pk}" class="link-danger">`,
-        `<i class="bi bi-dropbox"></i>`,
-        `</a>`
-    ].join('');
-};
 
-const jFLocalFooterFormatterFunc = (data) => {
-    return `<button class="btn btn-outline-success" type="button"
-                                        id="TableFooterSaveButtonId">Save</button>`;
+    if (row.IsSettled) {
+        return [
+            `<a href="./ExitOrderItems.html?OrderNumber=${row.pk}" class="link-danger">`,
+            `<i class="bi bi-dropbox"></i>`,
+            `</a>`
+        ].join('');
+
+    } else {
+        return [
+            `<a href="#" class="link-danger DeleteClass">`,
+            `<i class="bi bi-trash"></i>`,
+            `</a>`
+        ].join('');
+
+    }
+
+
 };
 
 export { StartFunc };
