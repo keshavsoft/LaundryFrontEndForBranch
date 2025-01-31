9207f53b97d9e3c4f366a334b23694c06ab9136b
sharath0036 <ksharathbabu2@gmail.com>
2025-01-31 16:15:12 +0530

    bug

--- src/Factory/ToFactory/SourceCode/Js/pages/QrCodesScan/Js/AddListeners/InputPkId/KeyPress/5-FetchFunc.js
+++ src/Factory/ToFactory/SourceCode/Js/pages/QrCodesScan/Js/AddListeners/InputPkId/KeyPress/5-FetchFunc.js
@@ -2,14 +2,10 @@ import { StartFunc as StartFuncFetchHeaders } from "./FetchHeaders/EntryFile.js"
 
 let StartFunc = async () => {
     let jVarLocalFetchHeaders = StartFuncFetchHeaders();
-
     let jVarLocalbranchName = localStorage.getItem("BranchName")
-    const modifiedBranch = jVarLocalbranchName.replace("BranOrders", "");
-
-    let jVarLocalFetchUrl = `/Custom/Cleaning/Branch/Factory/ToFactory/ScanV1/${modifiedBranch}`;
+    let jVarLocalFetchUrl = `/Custom/Cleaning/Branch/Factory/ToFactory/ScanV1/${jVarLocalbranchName}`;
 
     let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
-    // let data = await response.json();
 
     return await response;
 };
