8d1a2a59af6951b1bada0fb37791923f0cdfb63f
Nakka Lakshmi Chandana <lakshmichandananakka05@gmail.com>
2025-02-04 20:34:41 +0530

    Formatter

--- src/BranchToday/Branch/SourceCode/Js/pages/PrintQr/FormLoad/BuildBsTable/ForColumns/FormatterFuncs/entryFile.js
+++ src/BranchToday/Branch/SourceCode/Js/pages/PrintQr/FormLoad/BuildBsTable/ForColumns/FormatterFuncs/entryFile.js
@@ -1,8 +1,14 @@
 import { StartFunc as ColumnSerial } from "./ColumnSerial/entryFile.js";
+import { StartFunc as ColumnSymbol } from "./ColumnSymbol/entryFile.js";
 
 let StartFunc = ({ inColumns }) => {
     let LocalColumns = inColumns;
     let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial");
+    let LocalColumnSymbol = LocalColumns.find(element => element.field === "AggValues.SettlementAmount");
+
+    if (LocalColumnSymbol === undefined === false) {
+        ColumnSymbol({ inFindColumn: LocalColumnSymbol });
+    };
 
     if (LocalColumnOperateFine === undefined === false) {
         ColumnSerial({ inFindColumn: LocalColumnOperateFine });
