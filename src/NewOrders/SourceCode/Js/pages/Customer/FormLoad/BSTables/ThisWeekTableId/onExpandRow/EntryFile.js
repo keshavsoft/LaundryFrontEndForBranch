872f83a9b2fecf54b4784e9035d152bf5194418b
Srivasu215 <sri.vasu215@gmail.com>
2025-02-08 15:08:15 +0530

    YesterDay and week bugs

--- src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/BSTables/ThisWeekTableId/onExpandRow/EntryFile.js
+++ src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/BSTables/ThisWeekTableId/onExpandRow/EntryFile.js
@@ -1,7 +1,9 @@
 import { StartFunc as StartFuncOnExpandRow } from "./onExpandRow/EntryFile.js";
 import optionsJson from './options.json' with {type: 'json'};
+import { StartFunc as ForColumns } from "./ForColumns/entryFile.js";
 
 let StartFunc = (index, row, $detail) => {
+    
     let jVarLocalDataArray = row.AggValues.ItemsArray;
     let jVarLocalTemplateForSubTable = document.getElementById("TemplateForItemNames");
     let clone = jVarLocalTemplateForSubTable.content.cloneNode("true");
@@ -13,25 +15,9 @@ let StartFunc = (index, row, $detail) => {
 
     optionsJson.data = jVarLocalDataArray;
     optionsJson.onExpandRow = StartFuncOnExpandRow;
+    ForColumns({ inColumns: optionsJson.columns });
 
     jVarLocalInsideTable.bootstrapTable(optionsJson);
-};
-
-let StartFunc1 = (index, row, $detail) => {
-    let jVarLocalDataArray = row.AggValues.ItemsArray;
-    let jVarLocalTemplateForSubTable = document.getElementById("TemplateForItemNames");
-    let clone = jVarLocalTemplateForSubTable.content.cloneNode("true");
-
-    const s = new XMLSerializer();
-    const str = s.serializeToString(clone);
-
-    let jVarLocalInsideTable = $detail.html(str).find('table');
-
-    jVarLocalInsideTable.bootstrapTable({
-        data: jVarLocalDataArray,
-        detailView: true,
-        onExpandRow: StartFuncOnExpandRow
-    });
 };
 
 export { StartFunc };
