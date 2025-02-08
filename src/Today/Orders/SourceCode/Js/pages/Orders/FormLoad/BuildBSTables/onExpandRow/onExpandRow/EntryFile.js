0d0a982dde684850baefd952b236125984614b37
Achansandeep21 <93124032+Achansandeep21@users.noreply.github.com>
2025-02-08 18:53:22 +0530

    addons date if fixed

--- src/Today/Orders/SourceCode/Js/pages/Orders/FormLoad/BuildBSTables/onExpandRow/onExpandRow/EntryFile.js
+++ src/Today/Orders/SourceCode/Js/pages/Orders/FormLoad/BuildBSTables/onExpandRow/onExpandRow/EntryFile.js
@@ -1,12 +1,13 @@
 let StartFunc = (index, row, $detail) => {
-    let jVarLocalDataArray = row.AddOnDataAsArray;
+     
+    let jVarLocalDataArray = row.AddOnArray;
     let jVarLocalTemplateForSubTable = document.getElementById("TemplateForAddOns");
     let clone = jVarLocalTemplateForSubTable.content.cloneNode("true");
     const s = new XMLSerializer();
     const str = s.serializeToString(clone);
 
     let jVarLocalInsideTable = $detail.html(str).find('table');
-
+    
     jVarLocalInsideTable.bootstrapTable({
         data: jVarLocalDataArray
     })
