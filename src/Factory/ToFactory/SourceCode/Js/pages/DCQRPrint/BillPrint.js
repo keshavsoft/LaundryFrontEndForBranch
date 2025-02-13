6ccbcbc723702a59c16c4b7ec6b89c507212490e
sharath0036 <ksharathbabu2@gmail.com>
2025-02-13 18:13:48 +0530

    Dc Qrcode

---
+++ src/Factory/ToFactory/SourceCode/Js/pages/DCQRPrint/BillPrint.js
@@ -0,0 +1,10 @@
+import { StartFunc as StartFuncShowOnDom } from "./FetchFromFuncs/Entry.js";
+
+const StartFunc = () => {
+    let jVarLocalFromAdmin = true;
+    if (jVarLocalFromAdmin) {
+        StartFuncShowOnDom().then();
+    };
+};
+
+StartFunc();
