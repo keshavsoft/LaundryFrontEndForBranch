3d95b12e9aec186c5c3cfa134cdb76566b9e0b2c
sharath0036 <ksharathbabu2@gmail.com>
2025-02-14 18:18:05 +0530

    reports

--- src/Reports/QrCodes/SourceCode/Js/pages/QrAll/FormLoad/AddListeners/RefreshBSTableId/EntryFile.js
+++ src/Reports/QrCodes/SourceCode/Js/pages/QrAll/FormLoad/AddListeners/RefreshBSTableId/EntryFile.js
@@ -1,10 +1,10 @@
-import { StartFunc as StartFuncFuncToRun } from "./ButtonClickFunc.js";
+import { StartFunc as StartFuncfetchasGet } from "./FetchAsGet/entryFile.js";
 
 let StartFunc = () => {
-    let jVarLocalRefreshBSTableId = document.getElementById("RefreshBSTableId");
+    let jVarLocalRefreshBSTableId = document.getElementById("ShowButtonId");
 
     if (jVarLocalRefreshBSTableId === null === false) {
-        jVarLocalRefreshBSTableId.addEventListener("click", StartFuncFuncToRun);
+        jVarLocalRefreshBSTableId.addEventListener("click", StartFuncfetchasGet);
     };
 };
 
