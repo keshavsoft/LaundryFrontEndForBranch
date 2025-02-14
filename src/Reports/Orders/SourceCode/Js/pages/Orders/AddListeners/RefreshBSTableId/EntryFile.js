5aa109d2891d915160bf30016b0f95b7833b5a52
sharath0036 <ksharathbabu2@gmail.com>
2025-02-14 20:36:01 +0530

    reports

--- src/Reports/Orders/SourceCode/Js/pages/Orders/AddListeners/RefreshBSTableId/EntryFile.js
+++ src/Reports/Orders/SourceCode/Js/pages/Orders/AddListeners/RefreshBSTableId/EntryFile.js
@@ -1,7 +1,7 @@
 import { StartFunc as StartFuncFuncToRun } from "./FetchFromFuncs/Entry.js";
 
 let StartFunc = () => {
-    let jVarLocalCustomerNameInputId = "RefreshBSTableId";
+    let jVarLocalCustomerNameInputId = "ShowButtonId";
     let jVarLocalHtmlId = document.getElementById(jVarLocalCustomerNameInputId);
 
     jVarLocalHtmlId.addEventListener("click", StartFuncFuncToRun);
