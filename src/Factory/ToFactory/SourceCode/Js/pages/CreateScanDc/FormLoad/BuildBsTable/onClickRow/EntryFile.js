cb40d8de928af668af3937fd3477f2b9bca977ff
Srivasu215 <sri.vasu215@gmail.com>
2025-02-15 09:57:58 +0530

    Dc Send Condion

--- src/Factory/ToFactory/SourceCode/Js/pages/CreateScanDc/FormLoad/BuildBsTable/onClickRow/EntryFile.js
+++ src/Factory/ToFactory/SourceCode/Js/pages/CreateScanDc/FormLoad/BuildBsTable/onClickRow/EntryFile.js
@@ -2,21 +2,22 @@ import { StartFunc as StartFuncBranchScanAlter } from "./BranchScanAlter/EntryFi
 
 const StartFunc = (row, $element, field) => {
     if (field === "Send") {
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, Send to Factory"
-        }).then((result) => {
+        if (row.ItemDetails > 0) {
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, Send to Factory"
+            }).then((result) => {
 
-            if (result.isConfirmed) {
-                StartFuncBranchScanAlter({ inRow: row })
-            }
-        });
-
+                if (result.isConfirmed) {
+                    StartFuncBranchScanAlter({ inRow: row })
+                }
+            });
+        }
     }
 
 };
