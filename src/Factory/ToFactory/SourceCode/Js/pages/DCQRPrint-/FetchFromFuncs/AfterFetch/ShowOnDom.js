f0e0406c79dfb3190c2488ad224f59c7ac3782d0
sharath0036 <ksharathbabu2@gmail.com>
2025-02-15 20:16:44 +0530

    DcQrPrint

---
+++ src/Factory/ToFactory/SourceCode/Js/pages/DCQRPrint-/FetchFromFuncs/AfterFetch/ShowOnDom.js
@@ -0,0 +1,94 @@
+let StartFunc = ({ inDataToShow }) => {
+    let LocalDataToShow = inDataToShow.OrderData;
+
+    jfShow({ inDataToShow: LocalDataToShow });
+    GenerateQrCode({
+        inQrData: LocalDataToShow,
+        inCanvasId: document.getElementById("CanvasId")
+    });
+
+    var $table = $('#table')
+
+    $table.bootstrapTable("load", LocalDataToShow);
+};
+let GenerateQrCode = ({ inQrData = "", inCanvasId }) => {
+    var canvas = inCanvasId;
+    canvas.height = 11;
+    canvas.width = 11;
+    canvas.style.visibility = 'hidden';
+    let opts = {};
+    opts.text = `${inQrData.pk}~${inQrData.BranchName}~${inQrData.Date}~${inQrData.Description}`;
+    opts.bcid = "qrcode";
+    opts.scaleX = 4;
+    opts.scaleY = 4;
+    opts.rotate = "N";
+    try {
+        let ts0 = new Date;
+        bwipjs.toCanvas(canvas, opts);
+        show(ts0, new Date);
+    } catch (e) {
+        console.log("error : ", e);
+        return;
+    }
+    function show(ts0, ts1) {
+        canvas.style.visibility = 'visible';
+    }
+};
+
+const jfShow = ({ inDataToShow }) => {
+    let LocalDataToShow = inDataToShow;
+    jFLocalToInputVoucherNumberDCDetailsTextId({ inVoucherNumberDCDetailsTextId: LocalDataToShow.pk });
+    jFLocalToInputDateTextDCDetailsId({ inDateTextDCDetailsId: LocalDataToShow.Date });
+    jFLocalToInputDescriptionTextDCDetailsId({ inDescriptionTextDCDetailsId: LocalDataToShow.Description });
+    jFLocalToInputDCFactoryDCDetailsTextId({ inDCFactoryDCDetailsTextId: LocalDataToShow.Factory });
+    jFLocalToInputDCBranchDCDetailsTextId({inDCBranchDCDetailsTextId:LocalDataToShow.BranchName })
+    let LocalBranchName = document.getElementById("BranchName");
+    LocalBranchName.innerHTML = LocalBranchName.innerHTML.replace("{{Branch}}", LocalDataToShow.BranchName);
+    LocalBranchName.innerHTML = LocalBranchName.innerHTML.replace("{{VoucherNumber}}", LocalDataToShow.pk);
+}
+
+let jFLocalToInputVoucherNumberDCDetailsTextId = ({ inVoucherNumberDCDetailsTextId }) => {
+    let jVarLocalHtmlId = 'VoucherNumberDCDetailsTextId';
+    let jVarLocalVoucherNumberDCDetailsTextId = document.getElementById(jVarLocalHtmlId);
+
+    if (jVarLocalVoucherNumberDCDetailsTextId === null === false) {
+        jVarLocalVoucherNumberDCDetailsTextId.value = inVoucherNumberDCDetailsTextId;
+    };
+};
+
+let jFLocalToInputDateTextDCDetailsId = ({ inDateTextDCDetailsId }) => {
+    let jVarLocalHtmlId = 'DateTextDCDetailsId';
+    let jVarLocalDateTextDCDetailsId = document.getElementById(jVarLocalHtmlId);
+
+    if (jVarLocalDateTextDCDetailsId === null === false) {
+        jVarLocalDateTextDCDetailsId.value = inDateTextDCDetailsId;
+    };
+};
+
+let jFLocalToInputDescriptionTextDCDetailsId = ({ inDescriptionTextDCDetailsId }) => {
+    let jVarLocalHtmlId = 'DescriptionTextDCDetailsId';
+    let jVarLocalDescriptionTextDCDetailsId = document.getElementById(jVarLocalHtmlId);
+
+    if (jVarLocalDescriptionTextDCDetailsId === null === false) {
+        jVarLocalDescriptionTextDCDetailsId.value = inDescriptionTextDCDetailsId;
+    };
+};
+
+let jFLocalToInputDCFactoryDCDetailsTextId = ({ inDCFactoryDCDetailsTextId }) => {
+    let jVarLocalHtmlId = 'DCFactoryDCDetailsTextId';
+    let jVarLocalDCFactoryDCDetailsTextId = document.getElementById(jVarLocalHtmlId);
+
+    if (jVarLocalDCFactoryDCDetailsTextId === null === false) {
+        jVarLocalDCFactoryDCDetailsTextId.value = inDCFactoryDCDetailsTextId;
+    };
+};
+
+let jFLocalToInputDCBranchDCDetailsTextId = ({ inDCBranchDCDetailsTextId }) => {
+    let jVarLocalHtmlId = 'DCBranchCDetailsTextId';
+    let jVarLocalDCFactoryDCDetailsTextId = document.getElementById(jVarLocalHtmlId);
+
+    if (jVarLocalDCFactoryDCDetailsTextId === null === false) {
+        jVarLocalDCFactoryDCDetailsTextId.value = inDCBranchDCDetailsTextId;
+    };
+};
+export { StartFunc};
--- src/Factory/ToFactory/SourceCode/Js/pages/DCQRPrint/FetchFromFuncs/AfterFetch/ShowOnDom.js
+++ src/Factory/ToFactory/SourceCode/Js/pages/DCQRPrint/FetchFromFuncs/AfterFetch/ShowOnDom.js
@@ -1,44 +1,84 @@
 let StartFunc = ({ inDataToShow }) => {
-    let LocalDataToShow = inDataToShow;
-    // jfShow({ inDataToShow: LocalDataToShow });
+    let LocalDataToShow = inDataToShow.OrderData;
+
+    jfShow({ inDataToShow: LocalDataToShow });
     GenerateQrCode({
         inQrData: LocalDataToShow,
         inCanvasId: document.getElementById("CanvasId")
     });
+
+    var $table = $('#table');
+    if ($table.length) {
+        $table.bootstrapTable("load", LocalDataToShow);
+    }
 };
 
 let GenerateQrCode = ({ inQrData = "", inCanvasId }) => {
     var canvas = inCanvasId;
     canvas.height = 100;
-    canvas.width =100;
+    canvas.width = 100;
     canvas.style.visibility = 'hidden';
-
-    let opts = {};
-    opts.text = `${inQrData.pk}~${inQrData.BranchName}~${inQrData.Date}~${inQrData.Description}`;
-    opts.bcid = "qrcode";
-    opts.scaleX = 3;
-    opts.scaleY = 3;
-    opts.rotate = "N";
-
+    let opts = {
+        text: `${inQrData.pk}~${inQrData.BranchName}~${inQrData.Date}~${inQrData.Description}`,
+        bcid: "qrcode",
+        scaleX: 2,
+        scaleY: 2,
+        rotate: "N"
+    };
     try {
         let ts0 = new Date;
         bwipjs.toCanvas(canvas, opts);
         show(ts0, new Date);
     } catch (e) {
-        console.log("error : ", e);
-        return;
+        console.error("QR Code Generation Error:", e);
     }
-
     function show(ts0, ts1) {
         canvas.style.visibility = 'visible';
     }
 };
 
-// const jfShow = ({ inDataToShow }) => {
-//     let LocalDataToShow = inDataToShow;
-//     let LocalBranchName = document.getElementById("BranchName");
-//     LocalBranchName.innerHTML = LocalBranchName.innerHTML.replace("{{Branch}}", LocalDataToShow.BranchName);
-//     LocalBranchName.innerHTML = LocalBranchName.innerHTML.replace("{{VoucherNumber}}", LocalDataToShow.pk);
-// }
+const jfShow = ({ inDataToShow }) => {
+    
+    jFLocalToInputVoucherNumberDCDetailsTextId({ inVoucherNumberDCDetailsTextId: inDataToShow.pk });
+    jFLocalToInputDateTextDCDetailsId({ inDateTextDCDetailsId: inDataToShow.Date });
+    jFLocalToInputDescriptionTextDCDetailsId({ inDescriptionTextDCDetailsId: inDataToShow.Description });
+    jFLocalToInputDCFactoryDCDetailsTextId({ inDCFactoryDCDetailsTextId: inDataToShow.Factory });
+    jFLocalToInputDCBranchDCDetailsTextId({ inDCBranchDCDetailsTextId: inDataToShow.BranchName });
+};
+
+let jFLocalToInputVoucherNumberDCDetailsTextId = ({ inVoucherNumberDCDetailsTextId }) => {
+    let element = document.getElementById('VoucherNumberDCDetailsTextId');
+    if (element !== null) {
+        element.innerHTML = inVoucherNumberDCDetailsTextId;
+    }
+};
+
+let jFLocalToInputDateTextDCDetailsId = ({ inDateTextDCDetailsId }) => {
+    let element = document.getElementById('DateTextDCDetailsId');
+    if (element !== null) {
+        element.innerHTML = inDateTextDCDetailsId;
+    }
+};
+
+let jFLocalToInputDescriptionTextDCDetailsId = ({ inDescriptionTextDCDetailsTextId }) => {
+    let element = document.getElementById('DescriptionTextDCDetailsTextId');
+    if (element !== null) {
+        element.innerHTML = inDescriptionTextDCDetailsTextId;
+    }
+};
+
+let jFLocalToInputDCFactoryDCDetailsTextId = ({ inDCFactoryDCDetailsTextId }) => {
+    let element = document.getElementById('DCFactoryDCDetailsTextId');
+    if (element !== null) {
+        element.innerHTML = inDCFactoryDCDetailsTextId;
+    }
+};
+
+let jFLocalToInputDCBranchDCDetailsTextId = ({ inDCBranchDCDetailsTextId }) => {
+    let element = document.getElementById('DCBranchDCDetailsTextId');
+    if (element !== null) {
+        element.innerHTML = inDCBranchDCDetailsTextId;
+    }
+};
 
 export { StartFunc };
