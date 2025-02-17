a9e60067eb53f971ce112fb06197c5ddb1f7368f
kavycoder <nallakavya248@gmail.com>
2025-02-17 19:34:43 +0530

    badges in QRAllStatus html

--- src/Reports/QrCodes/SourceCode/Js/pages/QrAllStatus/FormLoad/BuildBsTable/ForColumns/ColumnStatus/entryFile.js
+++ src/Reports/QrCodes/SourceCode/Js/pages/QrAllStatus/FormLoad/BuildBsTable/ForColumns/ColumnStatus/entryFile.js
@@ -4,11 +4,9 @@ let StartFunc = ({ inFindColumn }) => {
 
 let jVarLocalFormatterFunc = (value, row, index) => {
     if (value === true) {
-        //return '<span class="badge bg-success"><i class="bi bi-check-lg"></i> <span class="d-none">Scanned</span></span>';
-
-        return '<span class="badge bg-success"><i class="bi bi-check-lg"></i> <span class="d-none">Scanned</span></span>';
+        return '<span class="badge bg-success"> ✔</span>';
     } else {
-        return '<span class="badge bg-danger"><i class="bi bi-x-lg"></i><span class="d-none">Pending</span></span>';
+        return '<span class="badge bg-danger">✘</span>';
     };
 };
 
--- src/Reports/QrCodes/SourceCode/Js/pages/QrAllStatus/FormLoad/BuildBsTable/ForColumns/EntryFile.js
+++ src/Reports/QrCodes/SourceCode/Js/pages/QrAllStatus/FormLoad/BuildBsTable/ForColumns/EntryFile.js
@@ -4,7 +4,7 @@ import { StartFunc as ColumnStatus } from "./ColumnStatus/entryFile.js";
 let StartFunc = ({ inColumns }) => {
     let LocalColumns = inColumns;
     let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial")
-    let LocalColumnStatus = LocalColumns.find(element => element.field === "Status");
+    let LocalColumnStatus = LocalColumns.find((element) => element.field === "Status");
 
     if (LocalColumnOperateFine === undefined === false) {
         ColumnOperate({ inFindColumn: LocalColumnOperateFine });
