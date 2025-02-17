2f3dbf926894ad01c6afb581eb3b93e51747f28b
Srivasu215 <sri.vasu215@gmail.com>
2025-02-17 10:24:17 +0530

    DC SHOW

--- src/Factory/ToFactory/SourceCode/Js/pages/CreateScanDc/FormLoad/BuildBsTable/onClickRow/BranchScanAlter/FetchFromFuncs/AfterFetch/EntryFile.js
+++ src/Factory/ToFactory/SourceCode/Js/pages/CreateScanDc/FormLoad/BuildBsTable/onClickRow/BranchScanAlter/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -2,6 +2,8 @@ import UrlJson from './url.json' with {type: 'json'};
 
 let StartFunc = ({ inRow }) => {
     let LocalPk = inRow.pk;
+    $('#table').bootstrapTable('refresh');
+
     window.open(`./${UrlJson.RedirectToUrl}?DCNumber=${LocalPk}`, '_blank');
 };
 
