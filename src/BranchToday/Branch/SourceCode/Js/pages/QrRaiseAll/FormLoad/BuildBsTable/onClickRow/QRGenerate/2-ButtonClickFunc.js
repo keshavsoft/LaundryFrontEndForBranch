0e414dbae9c606b35496dfbc617754e3d8f8d9ad
Srivasu215 <sri.vasu215@gmail.com>
2025-02-21 12:33:11 +0530

    Qr raise Alert

--- src/BranchToday/Branch/SourceCode/Js/pages/QrRaiseAll/FormLoad/BuildBsTable/onClickRow/QRGenerate/2-ButtonClickFunc.js
+++ src/BranchToday/Branch/SourceCode/Js/pages/QrRaiseAll/FormLoad/BuildBsTable/onClickRow/QRGenerate/2-ButtonClickFunc.js
@@ -1,8 +1,8 @@
 import { StartFunc as StartFuncFetchFunc } from "./5-FetchFunc.js";
 import { StartFunc as StartFuncCheckBeforeFetch } from "./3-CheckBeforeFetch.js";
+import { StartFunc as AfterFetch } from "./6-AfterFetch.js";
 
 let StartFunc = async ({ inRowData }) => {
-
     Swal.fire({
         title: 'Are you sure?',
         text: "You won't be able to revert this!",
@@ -13,18 +13,25 @@ let StartFunc = async ({ inRowData }) => {
         confirmButtonText: 'Ok',
         focusCancel: true,
         focusButtonColor: "#0000ff"
-    }).then((result) => {
-        if (result.isConfirmed) {
-            if (StartFuncCheckBeforeFetch()) {
-                StartFuncFetchFunc({ inRowData }).then(ResponseData => {
+    }).then(async (result) => {
+        if (result.isConfirmed && StartFuncCheckBeforeFetch()) {
+            Swal.fire({
+                title: "Generating QrCodes...",
+                html: "Please wait while we process your request.",
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading();
+                }
+            });
 
-                });
+            let ResponseData = await StartFuncFetchFunc({ inRowData });
 
-            };
+            if (ResponseData.status === 200) {
+                Swal.close();
+                AfterFetch({ inQrCodeData: ResponseData, inRow: inRowData });
+            }
         }
     });
-
-
 };
 
 export { StartFunc };
