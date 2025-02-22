f110b3d62884ac6f04877f5323dd0d1c7c4401b0
pushpadamalanka82 <y*********@gmail.com>
2025-02-22 13:03:44 +0530

    new ui

---
+++ src/DcSend/SourceCode/Js/pages/Index/FormLoad/StartFunc.js
@@ -0,0 +1,9 @@
+import { StartFunc as StartFuncFromUrlParams } from "./FromUrlParams/EntryFile.js";
+import { StartFunc as DcCount } from "./DcCount/EntryFile.js";
+
+const StartFunc = async () => {
+    StartFuncFromUrlParams();
+    DcCount();
+};
+
+export { StartFunc };
