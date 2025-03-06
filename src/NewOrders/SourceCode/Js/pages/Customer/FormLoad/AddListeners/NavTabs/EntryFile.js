388d00f2937d9f1442308f7398e54db1245cff3d
Nakka Lakshmi Chandana <lakshmichandananakka05@gmail.com>
2025-03-06 18:47:20 +0530

    All orders

--- src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/AddListeners/NavTabs/EntryFile.js
+++ src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/AddListeners/NavTabs/EntryFile.js
@@ -1,10 +1,12 @@
 import { StartFunc as YesterdayRefreshButtonId } from "./YesterdayRefreshButtonId/EntryFile.js";
 import { StartFunc as ThisWeekTableId } from "./ThisWeekTableId/EntryFile.js";
+import { StartFunc as AllTableId } from "./AllTableId/EntryFile.js";
 
 
 let StartFunc = () => {
     YesterdayRefreshButtonId();
-    ThisWeekTableId()
+    ThisWeekTableId();
+    AllTableId();
 };
 
 export { StartFunc }
