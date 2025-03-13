b3a6fa3396e106c6bba0d04285d7f1ba1c113ff1
Nakka Lakshmi Chandana <lakshmichandananakka05@gmail.com>
2025-03-13 17:17:32 +0530

    Header changes

--- src/Factory/ToFactory/SourceCode/Js/pages/Index/StartFunc.js
+++ src/Factory/ToFactory/SourceCode/Js/pages/Index/StartFunc.js
@@ -1,7 +1,9 @@
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
+import { StartFunc as LoginModal } from "../../../assets/static/js/Common/NiceAdmin/Header/LoginModal/EntryFile.js";
+
 
 const StartFunc = () => {
-    let jVarLocalFromAdmin = true;
+    let jVarLocalFromAdmin = LoginModal({ inSuccessFunc: StartFuncShowOnDom });
 
     if (jVarLocalFromAdmin) {
         jFLocalLeftSide();
