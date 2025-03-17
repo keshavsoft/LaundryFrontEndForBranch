ac6570a9a2db03666554a047292a79f52b41ca48
Nakka Lakshmi Chandana <lakshmichandananakka05@gmail.com>
2025-03-17 12:28:01 +0530

    Header login

--- src/NewOrders/SourceCode/assets/static/js/Common/NiceAdmin/Header/LoginModal/EntryFile.js
+++ src/NewOrders/SourceCode/assets/static/js/Common/NiceAdmin/Header/LoginModal/EntryFile.js
@@ -2,6 +2,7 @@ import { StartFunc as StartFuncCheckCookie } from "./CheckCookie.js";
 import { StartFunc as StartFuncSetFocus } from "./SetFocus.js";
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/EntryFile.js";
 import { StartFunc as ShowOnDom } from "./ShowOnDom/EntryFile.js";
+
 const StartFunc = ({ inSuccessFunc }) => {
     let localCheckCookie = StartFuncCheckCookie();
 
