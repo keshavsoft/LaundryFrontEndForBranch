b2be57843853dee968ee7acec2f9777fafca648e
sharath0036 <ksharathbabu2@gmail.com>
2025-03-17 10:28:14 +0530

    scan bug

--- src/Factory/ToFactory/SourceCode/Js/pages/QrCodesScan/Js/StartFunc.js
+++ src/Factory/ToFactory/SourceCode/Js/pages/QrCodesScan/Js/StartFunc.js
@@ -2,9 +2,8 @@ import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js"
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
 import { StartFunc as LoginModal } from "../../../../assets/static/js/Common/NiceAdmin/Header/LoginModal/EntryFile.js";
 
-
 const StartFunc = () => {
-   let jVarLocalFromAdmin = LoginModal({ inSuccessFunc: StartFuncShowOnDom });
+   let jVarLocalFromAdmin = LoginModal({ inSuccessFunc: StartFuncFormLoad });
 
    if (jVarLocalFromAdmin) {
       StartFuncFormLoad();
