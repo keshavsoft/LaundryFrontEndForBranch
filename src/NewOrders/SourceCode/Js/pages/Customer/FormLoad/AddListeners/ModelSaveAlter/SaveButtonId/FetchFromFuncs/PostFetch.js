96b4e7cd59e23944aa173ddde1093ab725106542
pushpadamalanka82 <y*********@gmail.com>
2025-04-01 19:47:59 +0530

    changes

--- src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/AddListeners/ModelSaveAlter/SaveButtonId/FetchFromFuncs/PostFetch.js
+++ src/NewOrders/SourceCode/Js/pages/Customer/FormLoad/AddListeners/ModelSaveAlter/SaveButtonId/FetchFromFuncs/PostFetch.js
@@ -3,11 +3,11 @@ import UrlJson from "../../../../Config.json" with { type: "json" };
 
 let StartFunc = async () => {
     let LocalroutePath = UrlJson.routePath;
-    let LocaltableName = UrlJson.tableName;
+    // let LocaltableName = UrlJson.tableName;
     let jVarLocalFilterString = jFLocalpkId();
 
     let jVarLocalFetchHeaders = StartFuncFetchHeaders();
-    let jVarLocalFetchUrl = `/${LocalroutePath}/${LocaltableName}/Alter/${jVarLocalFilterString}`;
+    let jVarLocalFetchUrl = `/${LocalroutePath}/${jVarLocalFilterString}`;
     let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
 
     return await response;
