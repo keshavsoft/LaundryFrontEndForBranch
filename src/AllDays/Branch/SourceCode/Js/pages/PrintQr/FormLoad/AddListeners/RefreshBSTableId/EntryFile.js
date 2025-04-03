04b3c98b2dee686cad48b528fe8c309396165f9b
pushpadamalanka82 <y*********@gmail.com>
2025-04-03 19:25:37 +0530

    some changes

--- src/AllDays/Branch/SourceCode/Js/pages/PrintQr/FormLoad/AddListeners/RefreshBSTableId/EntryFile.js
+++ src/AllDays/Branch/SourceCode/Js/pages/PrintQr/FormLoad/AddListeners/RefreshBSTableId/EntryFile.js
@@ -1,7 +1,7 @@
 import { StartFunc as StartFuncFuncToRun } from "./ButtonClickFunc.js";
 
 let StartFunc = () => {
-    let jVarLocalRefreshBSTableId = document.getElementById("RefreshBSTableId");
+    let jVarLocalRefreshBSTableId = document.getElementById("ShowId");
 
     if (jVarLocalRefreshBSTableId === null === false) {
         jVarLocalRefreshBSTableId.addEventListener("click", StartFuncFuncToRun);
