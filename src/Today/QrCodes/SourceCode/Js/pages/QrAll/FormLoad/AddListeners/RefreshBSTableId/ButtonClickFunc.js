986831524b59a71464f61b1309c7efe5684fbb11
Srivasu215 <sri.vasu215@gmail.com>
2025-04-10 11:28:12 +0530

    Today Qr All

--- src/Today/QrCodes/SourceCode/Js/pages/QrAll/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
+++ src/Today/QrCodes/SourceCode/Js/pages/QrAll/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
@@ -1,16 +1,13 @@
-import { StartFunc as Receipts } from "./Receipts/Entry.js";
+import { StartFunc as QrCodes } from "./QrCodes/Entry.js";
 
 import { StartFunc as StartFuncAfterFetch } from "./AfterFetch/EntryFile.js";
 
 let StartFunc = async () => {
     try {
-        let [a] = await Promise.all([Receipts()]);
+        let a = QrCodes();
 
-        jVarGlobalPresentViewData = [...a];
-
-        StartFuncAfterFetch();
+        StartFuncAfterFetch(a);
     } catch (e) {
-        console.log("error from Promise all : ", e);
         alert("error from Promise All");
     };
 };
