c1f676450ffead17bde37e4a371d2802bdee5f11
Anantha123761 <anatalakshmikurupudi2002@gmail.com>
2025-04-15 19:17:03 +0530

    Branch  At reports FromFactory Folder create

---
+++ src/Reports/QrCodes/SourceCode/Js/pages/QRCompleteFilter-/StartFunc.js
@@ -0,0 +1,13 @@
+import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
+import { StartFunc as LoginModal } from "../../../assets/static/js/Common/NiceAdmin/Header/LoginModal/EntryFile.js";
+
+const StartFunc = () => {
+    StartFuncFormLoad();
+    let jVarLocalFromAdmin = LoginModal({ inSuccessFunc: StartFuncFormLoad });
+    
+    if (jVarLocalFromAdmin) {
+       // StartFuncShowOnDom().then();
+    };
+};
+
+StartFunc();
