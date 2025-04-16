986eae0566e697ffbee84ffe1660c776969d511c
sharath0036 <ksharathbabu2@gmail.com>
2025-04-16 20:19:08 +0530

    Ui For FromFactory QrCodes in reports

--- src/Reports/QrCodes/SourceCode/Js/pages/QRPendingFilter/FormLoad/BuildBSTables/ForColumns/EntryFile.js
+++ src/Reports/QrCodes/SourceCode/Js/pages/QRPendingFilter/FormLoad/BuildBSTables/ForColumns/EntryFile.js
@@ -1,17 +1,23 @@
 import { StartFunc as ColumnRate } from "./ColumnRate/entryFile.js";
 import { StartFunc as ColumnSerial } from "./ColumnSerial/entryFile.js";
+import { StartFunc as ColumnStatus } from "./ColumnStatus/entryFile.js";
 
 let StartFunc = ({ inColumns }) => {
     let LocalColumns = inColumns;
     let LocalColumnRate = LocalColumns.find(element => element.field === "Rate");
+    let LocalColumnStatus = LocalColumns.find(element => element.field === "Status");
+    let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial")
 
     if (LocalColumnRate === undefined === false) {
         ColumnRate({ inFindColumn: LocalColumnRate });
     };
 
-    let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial")
     if (LocalColumnOperateFine === undefined === false) {
         ColumnSerial({ inFindColumn: LocalColumnOperateFine });
+    };
+
+    if (LocalColumnStatus === undefined === false) {
+        ColumnStatus({ inFindColumn: LocalColumnStatus });
     };
 };
 export { StartFunc };
